13e309f4d954f79c80650ebd3c99cb643e9a7aa6
BatuHanHub <114951572+BatuHanHub@users.noreply.github.com>
2023-05-29 18:57:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adınız ne?");
+            string ad = Console.ReadLine();
+            Console.WriteLine("Merhaba " + ad + " nasılsın?");
+            string nasil = Console.ReadLine();
+            Console.WriteLine("Bende " + nasil+" kaç yaşındasınız?");
+            int yas = Convert.ToInt32(Console.ReadLine());
+            int yas_farki = yas - 15;
+            Convert.ToString(yas_farki);
+
+            if (yas_farki > yas)
+            {
+                Console.WriteLine("Aramızda "+ yas_farki);
+            }
+            else if (yas == 15)
+            {
+                Console.WriteLine("Yaşıtız");
+            }
+            
+            else
+            {
+                Console.WriteLine("Küçüğümsün");
+            }
+            Console.WriteLine("Kaç tane programlama dili biliyorsun");
+            int dil = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ben 5 tane dil biliyorum\nPython\nC/C++/C#\nPascal");
+        }
+    }
+}
